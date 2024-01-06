c7cbe391c518f0d6e7742b81c1b7b16be9b70dae
Dylan <dylanlancekemboi@gmail.com>
2024-01-06 11:58:43 +0300

    Completed Lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,7 +1,5 @@
 class MySet:
-    def __init__(self, enumerable=None):
-        if enumerable is None:
-            enumerable = []
+    def __init__(self, enumerable = []):
         self.dictionary = {}
         for value in enumerable:
             self.dictionary[value] = True
