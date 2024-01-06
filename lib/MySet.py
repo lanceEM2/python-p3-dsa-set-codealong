69820e780b2c6d64ce5e5930c1633d43f3959a89
Dylan <dylanlancekemboi@gmail.com>
2024-01-06 11:49:15 +0300

    Completed Lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,24 @@
 class MySet:
+    def __init__(self, enumerable=None):
+        if enumerable is None:
+            enumerable = []
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True   # Add a value as a key on the dictionary
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        return self.dictionary.clear()
